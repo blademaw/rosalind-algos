4d7d8dbada43a2508b16ec0d78b5c9bd0ac00a0f
blademaw <jackoli3@me.com>
2023-11-22 22:16:11 +1100

    Add adjacency list conversion function

--- utils/util.go
+++ utils/util.go
@@ -1,6 +1,9 @@
 package utils
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 // Converts a list of strings to list of integers.
 func StringArrToInts(arr []string) []int {
@@ -12,3 +15,22 @@ func StringArrToInts(arr []string) []int {
 
 	return res
 }
+
+// Converts a list of strings in edge list format to an adjacency
+// list. Takes `n`, the number of nodes, `arr`, the list of
+// strings. Assumes nodes are 1-based indexed.
+func EdgeListToAdjacencyList(n int, arr []string) [][]int {
+  edges := make([][]int, n)
+  for _, edge := range arr {
+    nodes := strings.Split(edge, " ")
+    n1, _ := strconv.Atoi(nodes[0])
+    n2, _ := strconv.Atoi(nodes[1])
+
+    n1--; n2--
+    edges[n1] = append(edges[n1], n2)
+    edges[n2] = append(edges[n2], n1)
+  }
+  
+  return edges
+}
+
